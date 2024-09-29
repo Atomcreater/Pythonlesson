ab85606fb552a3d97aac60f927955ef0c05e8a80
unknown <artemmudrov298@gmail.com>
2024-09-30 01:10:56 +0300

    task3

--- 2 week/Task 3.py
+++ 2 week/Task 3.py
@@ -1,1 +1,18 @@
+#187. Repeated DNA Sequences
+#https://leetcode.com/problems/repeated-dna-sequences/description/?envType=problem-list-v2&envId=string
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        def findRepeatedDnaSequences(s):
+            seen = set()
+            repeated = set()
 
+            for i in range(len(s) - 9):
+                sequence = s[i:i + 10]
+                if sequence in seen:
+                    repeated.add(sequence)
+                else:
+                    seen.add(sequence)
+
+            return list(repeated)
+
+        return(findRepeatedDnaSequences(s))
