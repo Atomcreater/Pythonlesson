a01e8bf213ec23f2b0c057f36cd9178e8f5a2c22
unknown <artemmudrov298@gmail.com>
2024-11-18 14:07:00 +0300

    task3

---
+++ 6 week/Task 3.py
@@ -0,0 +1,17 @@
+#658. Find K Closest Elements
+#https://leetcode.com/problems/find-k-closest-elements/description/?envType=problem-list-v2&envId=sliding-window
+from typing import List
+
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        left, right = 0, len(arr) - k
+
+        while left < right:
+            mid = (left + right) // 2
+
+            if x - arr[mid] <= arr[mid + k] - x:
+                right = mid
+            else:
+                left = mid + 1
+
+        return arr[left:left + k]
